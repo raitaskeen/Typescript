ab945ae32c982542a21f9f016a47509dd8d105fe
Taskeen <raitaskeenhaider786@gmail.com>
2024-01-18 00:00:03 +0500

    Add func.js and func.ts files

---
+++ src/func.ts
@@ -0,0 +1,8 @@
+// main.ts
+function calculateArea(length: number, width: number): number {
+    return length * width;
+  }
+  
+  const areas = calculateArea(10, 5);
+  console.log(`Area of the rectangle: ${areas}`);
+  
